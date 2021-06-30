5adbee40b8a7c9d5f1b4b1e5d08e38378b6fd3e9
jin0220 <37447282+jin0220@users.noreply.github.com>
2021-06-30 18:35:43 +0900

    main
    메인 모니터링 그래프 추가

--- app/src/main/java/com/example/farm_monitoring/MonitoringFragment.java
+++ app/src/main/java/com/example/farm_monitoring/MonitoringFragment.java
@@ -1,26 +1,40 @@
 package com.example.farm_monitoring;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MonitoringFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class MonitoringFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +42,6 @@ public class MonitoringFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MonitoringFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MonitoringFragment newInstance(String param1, String param2) {
         MonitoringFragment fragment = new MonitoringFragment();
         Bundle args = new Bundle();
@@ -58,7 +63,82 @@ public class MonitoringFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_monitoring, container, false);
+        View view = inflater.inflate(R.layout.fragment_monitoring, container, false);
+
+        LineChart lineChart1 = view.findViewById(R.id.lineChart);
+        lineChart1.setTouchEnabled(false);
+
+        XAxis xAxis = lineChart1.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setLabelCount(8,true);
+        xAxis.setTextSize(11f);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+
+                SimpleDateFormat sdf = new SimpleDateFormat("H");
+                Log.d("확인",value +"");
+//                long emissionsMilliSince1970Time = ((long) value) * 1000;
+//
+//                // Show time in local version
+//                Date timeMilliseconds = new Date(emissionsMilliSince1970Time);
+//                DateFormat dateTimeFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault());
+
+//                return dateTimeFormat.format(timeMilliseconds) + "시";
+                return sdf.format(value) + "시";
+            }
+        });
+
+        YAxis leftAxis = lineChart1.getAxisLeft();
+        leftAxis.setTextColor(Color.BLACK);
+        leftAxis.setLabelCount(10,true);
+        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGranularityEnabled(true);
+
+        YAxis rightAxis = lineChart1.getAxisRight();
+        rightAxis.setDrawLabels(false);
+        rightAxis.setDrawAxisLine(false);
+        rightAxis.setDrawGridLines(false);
+
+
+        List<Entry> entry_chart = new ArrayList<>();
+
+        LineData charData = new LineData();
+
+        entry_chart.add(new Entry(0,5));
+        entry_chart.add(new Entry(3,6));
+        entry_chart.add(new Entry(6,7));
+        entry_chart.add(new Entry(9,3));
+
+        LineDataSet lineDataSet = new LineDataSet(entry_chart, "차트1");
+        lineDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        lineDataSet.setColor(Color.BLUE);
+        lineDataSet.setCircleColor(Color.BLUE);
+        lineDataSet.setLineWidth(2f);
+        lineDataSet.setCircleRadius(3f);
+        lineDataSet.setFillAlpha(65);
+//        lineDataSet.setFillColor(Color.RED);
+        lineDataSet.setDrawCircleHole(false);
+//        lineDataSet.setHighLightColor(Color.rgb(244, 117, 117));
+        lineDataSet.setCircleRadius(6); // 곡률
+
+
+
+        charData.addDataSet(lineDataSet);
+
+        lineChart1.setDescription(null);
+
+        lineChart1.setData(charData);
+
+
+        lineChart1.invalidate();
+
+        return view;
     }
+
 }
