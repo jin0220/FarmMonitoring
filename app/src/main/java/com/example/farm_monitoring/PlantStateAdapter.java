3fc5451f0fa74e6200b812087a9a135de06d2821
jin0220 <37447282+jin0220@users.noreply.github.com>
2021-07-20 16:59:59 +0900

    plant_state
    재배 관리 페이지 ui 수정

---
+++ app/src/main/java/com/example/farm_monitoring/PlantStateAdapter.java
@@ -0,0 +1,63 @@
+package com.example.farm_monitoring;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlantStateAdapter extends RecyclerView.Adapter<PlantStateAdapter.ViewHolder>{
+    TextView plant_name, start_date, end_date;
+    List<PlantStateData> items = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.recyclerview_content_plant, parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        PlantStateData plantStateData = items.get(position);
+        plant_name.setText(plantStateData.getPlant_name());
+        start_date.setText(plantStateData.getStart_date());
+        end_date.setText(plantStateData.getEnd_date());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            plant_name = itemView.findViewById(R.id.plant_name);
+            start_date = itemView.findViewById(R.id.start_date);
+            end_date = itemView.findViewById(R.id.end_date);
+
+        }
+    }
+    public void addData(String plant_name, String start_date, String end_date){
+        PlantStateData item = new PlantStateData();
+
+        item.setPlant_name(plant_name);
+        item.setStart_date(start_date);
+        item.setEnd_date(end_date);
+
+        items.add(item);
+    }
+}
--- app/src/main/java/com/example/farm_monitoring/PlantStateFragment.java
+++ app/src/main/java/com/example/farm_monitoring/PlantStateFragment.java
@@ -3,24 +3,18 @@ package com.example.farm_monitoring;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PlantStateFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PlantStateFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +22,6 @@ public class PlantStateFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlantStateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static PlantStateFragment newInstance(String param1, String param2) {
         PlantStateFragment fragment = new PlantStateFragment();
         Bundle args = new Bundle();
@@ -58,7 +43,19 @@ public class PlantStateFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plant_state, container, false);
+        View view = inflater.inflate(R.layout.fragment_plant_state, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        PlantStateAdapter adapter = new PlantStateAdapter();
+        recyclerView.setAdapter(adapter);
+
+        for(int i=0;i<10;i++) {
+            adapter.addData("당근", "2021.06.01", "2021.06.30");
+        }
+
+        return view;
     }
 }
